4ec2e55561c6436c3b230ac702d2b4e8c2a08b53
Joscoo <104363551+Joscoo@users.noreply.github.com>
2023-04-19 12:07:36 +0000

    Clase Palindromo

--- 18-4-23/cadenas.cpp
+++ 18-4-23/cadenas.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
-int main(){
-    char cadena[10];
-    cin >> cadena;
-    cout << cadena;
-    
+bool is_palindrome(char arr[], int start, int end) {
+    if (start >= end) {
+        return true;
+    }
+    if (arr[start] != arr[end]) {
+        return false;
+    }
+    return is_palindrome(arr, start+1, end-1);
+}
 
+int main() {
+    const int MAX_SIZE = 100;
+    char arr[MAX_SIZE];
+    cout << "Ingresa los caracteres del arreglo: ";
+    cin.getline(arr, MAX_SIZE);
+    int start = 0;
+    int end = strlen(arr) - 1;
+    if (is_palindrome(arr, start, end)) {
+        cout << "El arreglo es un palindromo" << endl;
+    } else {
+        cout << "El arreglo no es un palindromo" << endl;
+    }
     return 0;
 }
