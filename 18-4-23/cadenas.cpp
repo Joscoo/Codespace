8cff8408febe7668705562556e5fe1ae4635aff8
Joscoo <104363551+Joscoo@users.noreply.github.com>
2023-04-19 13:20:02 +0000

    avance palindromo

--- 18-4-23/cadenas.cpp
+++ 18-4-23/cadenas.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
 #include <cstring>
+#define EXT 15
 
 using namespace std;
 
-bool is_palindrome(char arr[], int start, int end) {
+bool esPalindromo(char vector[], int start, int end) {
     if (start >= end) {
         return true;
     }
-    if (arr[start] != arr[end]) {
+    if (vector[start] != vector[end]) {
         return false;
     }
-    return is_palindrome(arr, start+1, end-1);
+    return esPalindromo(vector, start+1, end-1);
+}
+
+void pasarAPalabras(char vector[], char p[][EXT], int * nPalabras){
+    int i, j = 0, k = 0;
+    for(i = 0; i < strlen(vector); i++){
+        if((vector[i] > 'A' && vector[i] <= 'Z') || (vector[i] > 'a' && vector[i] <= 'z')){
+            p[j][k] = vector[i];
+            k++;
+        }else{
+            p[j][k] = '\0';
+            j++;
+        }
+    }
 }
 
 int main() {
-    const int MAX_SIZE = 100;
-    char arr[MAX_SIZE];
+    char vector[EXT];
+    char palabras[EXT/2][EXT];
+    int nPalabras;
+
     cout << "Ingresa los caracteres del arreglo: ";
-    cin.getline(arr, MAX_SIZE);
+    cin.getline(vector, EXT);
     int start = 0;
-    int end = strlen(arr) - 1;
-    if (is_palindrome(arr, start, end)) {
-        cout << "El arreglo es un palindromo" << endl;
-    } else {
-        cout << "El arreglo no es un palindromo" << endl;
+    int end = strlen(vector) - 1;
+
+    pasarAPalabras(vector, palabras, &nPalabras);
+
+    for(int i = 0; i < nPalabras; i++){
+        if (esPalindromo(palabras[i], start, end)) {
+            cout << "La palabra: " << palabras[i] <<" es un palindromo" << endl;
+        } else {
+            cout << "La palabra: " << palabras[i] <<" no es un palindromo" << endl;
+        }
     }
+
     return 0;
 }
