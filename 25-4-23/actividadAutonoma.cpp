65577c61a61653908534b621a424e383e9ae8902
Joscoo <104363551+Joscoo@users.noreply.github.com>
2023-04-25 17:45:25 +0000

    autonoma

---
+++ 25-4-23/actividadAutonoma.cpp
@@ -0,0 +1,62 @@
+//Jose Bonilla - 7246
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    //Variables
+    int n; // Cantidad de valores a calcular
+
+    //Prototipos de funcion
+    void potenciacion(int n);
+    int leerN();
+
+    //Código
+    n = leerN();
+
+    potenciacion(n);
+
+    return 0;
+}
+
+int leerN(){
+    int n;
+    cout << "Ingrese un valor positivo" << endl;
+    cin >> n;
+    if(n < 0){
+        n = leerN();
+    }
+    return n;
+}
+
+void potenciacion(int n){
+    int sumaInterna(int a, int resul, int auxA);
+    if(n > 0){
+        cout << "Base: "<< endl;
+        int a = leerN();
+        cout << "Exponente: " << endl;
+        int b = leerN(); 
+        if(b == 1){
+            cout << "Resultado: " << a << endl;        
+        }else if(b == 0){
+            cout << "Resultado: " << 0 << endl;        
+        }else{           
+            int resulInterno, resulTotal;
+            resulInterno = sumaInterna(a, 0, a);
+            resulTotal = sumaInterna(resulInterno, 0, b - 1);
+            cout << "Resultado: " << resulTotal << endl;        
+            potenciacion(n - 1);
+        }
+    }
+}
+
+
+int sumaInterna(int a, int resul, int auxA){
+    if (auxA > 0){
+        resul = resul + a ; 
+        sumaInterna(resul, resul, auxA - 1);
+    }else{
+        return resul;
+    }
+}
+
