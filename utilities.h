9eb14f25edacca9d6b9960f067416aab3cce859d
Joscoo <104363551+Joscoo@users.noreply.github.com>
2023-05-03 03:14:42 +0000

    repaso

--- utilities.h
+++ utilities.h
@@ -1,6 +1,8 @@
 //Funciones utiles para reutilizar
 #include <iostream>
 using namespace std;
+
+
 int leerNEnteroPositivo(){
     int numero;
     cout << "Ingrese un numero entero positivo " << endl;
@@ -78,4 +80,48 @@ void imprimirVectorRecursivamente(int *vector, int n, int index) {
     }
     cout << vector[index] << endl;
     imprimirVectorRecursivamente(vector, n, index + 1);
+}
+
+int calculoFactorial(int n){
+    if (n == 0){
+        return 1;
+    }else{
+        return n * calculoFactorial(n-1);
+    }
+}
+
+int fibonacci(int n){
+    if(n <= 1){
+        return n;
+    }else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    
+}
+
+void imprimirFibonacci(int n) {
+    if (n <= 0) {
+        return;
+    } else {
+        imprimirFibonacci(n-1);
+        cout << fibonacci(n-1) << " ";
+    }
+}
+
+int sumaDivisores(int numero, int divisor){
+    if (divisor == 0) {
+        return 0;
+    }
+    if (numero % divisor == 0 && numero != divisor) {
+        return divisor + sumaDivisores(numero, divisor - 1);
+    }
+    return sumaDivisores(numero, divisor - 1);
+}
+
+bool confirmarPerfecto(int suma, int n){
+    if(n == suma){
+        return true;
+    }else{
+        return false;
+    }
 }
