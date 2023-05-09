79cd852b5a957a080a2d2150cb01c5357bec62da
Joscoo <104363551+Joscoo@users.noreply.github.com>
2023-05-09 17:43:38 +0000

    Pilas

--- Estructuras de Datos Lineales/Pilas/ejercicioPilas.cpp
+++ Estructuras de Datos Lineales/Pilas/ejercicioPilas.cpp
@@ -4,23 +4,43 @@
 using namespace std;
 
 int main(){
-    system("color 02");
+    // system("color 02");
     Pila p;
     
-    void leerPila(Pila p1);
+    void leerPila(Pila *p1);
     void mostrarPila(Pila p1);
 
-    leerPila(p);
+    leerPila(&p);
     mostrarPila(p);
 
-    system("Pause");
+    // system("Pause");
     return 0;
 }
 
-void leerPila(Pila p1){
-
+void leerPila(Pila *p1){
+    Tipo valor;
+    int opc = 2;
+    do{
+        if(!p1->pilaLlena()){
+            cout << "Ingrese un valor para la pila" << endl;    
+            cin >> valor;
+            p1->push(valor);
+            cout << endl << "Desea seguir ingresando valores?" << endl;
+            cout << "1. Si || 2. No (Ingrese 1 o 2)" << endl;
+            cin >> opc; 
+        }else{
+            cout << endl << "La pila esta llena" << endl;
+            opc = 2;
+        }
+    }while(opc == 1);
 }
 
 void mostrarPila(Pila p1){
-
+    Tipo aux;
+    while(!p1.pilaVacia()){
+        if(!p1.pilaVacia()){
+            aux = p1.pop();
+            cout << endl << "El valor es: "<< aux << endl;
+        }
+    }
 }
