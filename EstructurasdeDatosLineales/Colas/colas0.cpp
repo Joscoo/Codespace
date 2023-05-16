e8e3db312f045420bc93551ea9c13e5c3850d21e
Joscoo <104363551+Joscoo@users.noreply.github.com>
2023-05-16 16:27:41 +0000

    Ingreso y salida de elementos de la cola

--- EstructurasdeDatosLineales/Colas/colas0.cpp
+++ EstructurasdeDatosLineales/Colas/colas0.cpp
@@ -5,17 +5,43 @@
 #include <iostream>
 #include "colas.h"
 
+#define FINAL 99999
 using namespace std;
 
 int main(){
     system("color 02");
+    
     Cola c;
-    cout << endl << "Elementos de la cola " << c.getFrente() << endl;
-    cout << endl << "El TDA" << (c.colaLlena()? " SI " : " NO ") << "esta llena" << endl;
-    cout << "Tamaño de cola:" << c.sizeCola() << endl;
-    c.setFinal(EXT - 1);
-    cout << endl << "El TDA" << (c.colaLlena()? " SI " : " NO ") << "esta llena" << endl;
-    cout << "Tamaño de cola:" << c.sizeCola() << endl;
+    void llenarCola(Cola *c);
+    void mostrarCola(Cola c, int i = 0);
+
+    cout << endl << "Ingreso de valores para la cola" << endl;
+    llenarCola(&c);
+    cout << "La cola " << (c.colaVacia()? " SI " : " NO ") << "esta vacia" << endl;
+    cout << "La cola tiene: " << c.sizeCola() << " elementos" << endl;
+    cout << "Los elementos de la cola son:" << endl;
+    mostrarCola(c);
+    cout << endl;
     system("pause");
     return 0;
 }
+
+void mostrarCola(Cola c, int i = 0){
+    if(!c.colaVacia()){
+        cout << endl << "[ " << i + 1 <<" ] " << c.dequeue() << endl;
+        mostrarCola(c, i + 1);
+    }
+}
+
+void llenarCola(Cola *c){
+    if(!c->colaLlena()){
+            Tipo valor;
+            cout << "Ingrese el valor para agregarlo a la cola (Digite 99999 para salir)" << endl;
+            cin >> valor;
+        if(valor != FINAL){
+            c->enqueue(valor);
+            llenarCola(c);
+        }
+    }
+}
+
