3f3a9d695ad6c066cae1fa5920dba22d3ff31acc
Joscoo <104363551+Joscoo@users.noreply.github.com>
2023-05-24 13:50:16 +0000

    Creacion de Nodos y Listas en C++

--- EstructurasdeDatosLineales/Listas/lista.h
+++ EstructurasdeDatosLineales/Listas/lista.h
@@ -10,17 +10,34 @@ public:
     Lista();
     void setPrimero(Nodo *p);
     Nodo* getPrimero();
-
+    bool esVacia();
+    void vaciarLista();
+    void insertarInicio(Tipo v);
 };
 
 Lista::Lista(){
     setPrimero(NULL);
 }
+
 void Lista::setPrimero(Nodo *p){
     primero = p;
 }
+
 Nodo* Lista::getPrimero(){
     return primero;
+}
+
+bool Lista::esVacia(){
+    return (getPrimero() == NULL);
+}
+
+void Lista::vaciarLista(){
+    setPrimero(NULL);
+}
+
+void Lista::insertarInicio(Tipo v){
+    Nodo *nuevo = new Nodo(v, getPrimero());
+    setPrimero(nuevo);
 }
 
 
--- EstructurasdeDatosLineales/Listas/lista1.cpp
+++ EstructurasdeDatosLineales/Listas/lista1.cpp
@@ -1,15 +1,33 @@
 using namespace std;
 #include <iostream>
 #include "lista.h"
+#define EXT 99999
 
 int main(){
     system("color 02");
     Lista l;
+    void ingresarLista(Lista *l1);
+    void mostrarLista(Lista l1);
 
     cout << endl << "El valor apuntado en el primer nodo es : " << l.getPrimero() << endl;
+    cout << endl << "La lista" << (l.esVacia() ? " si " :  " no ") <<  "esta vacia"  << endl;
 
-
+    cout << "Valores a ingresar en la lista : " << endl; 
+    ingresarLista(&l);
 
     system("pause");
     return 0;
+}
+
+void ingresarLista(Lista *l1){
+    Tipo aux;
+
+    cout << endl << "Ingrese un valor para la lista (" << EXT << " para terminar) : " << endl; 
+    cin >> aux;
+    
+    if(aux != EXT){
+        l1->insertarInicio(aux);
+        ingresarLista(l1);
+    }
+
 }
